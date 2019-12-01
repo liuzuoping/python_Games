a6fd53180c17a84ed305d28c8380b4186848c263
Jack Liu <2330163321@qq.com>
2019-12-01 13:07:14 +0800

    fifth commit

--- alien_invasion/game_stats.py
+++
@@ -1,7 +0,0 @@
-class GameStats():
-    def __init__(self,ai_settings):
-        self.ai_settings=ai_settings
-        self.reset_stats()
-        self.game_active=True
-    def reset_stats(self):
-        self.ships_left=self.ai_settings.ship_limit
